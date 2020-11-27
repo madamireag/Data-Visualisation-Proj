4ad70bfb310ee3f20aadd5a3da41dbeb302ed2a1
Mireag Madalina <mireagmada@gmail.com>
2020-11-27 18:22:15 +0200

    i think i fixed the table

--- 3_1082_Mireag_Madalina_Andreea.js
+++ 3_1082_Mireag_Madalina_Andreea.js
@@ -11,7 +11,7 @@ function drawTable(dataset, anSelectat) {
       document.body.removeChild(existentTable);
 
    let table = document.createElement("table");
-   table.setAttribute("id", "dataTable");
+   table.setAttribute("id", "dataTable"); //ii dau id ca sa-l pot stiliza in css
    table.classList.add("table");
    //creare header tabel
    let tHead = document.createElement("thead");
@@ -40,34 +40,64 @@ function drawTable(dataset, anSelectat) {
    let tdPIB;
    let tdPop;
    let tableData = [];
+   //filtrez datele si le iau doar pe cele care corespund anului selectat
    tableData = dataset.filter(e => e.an === anSelectat);
-   console.log(tableData)
-   for (i = 0; i < tableData.length; i++) {
-
+   //iau toate tarile intr-un vector(sunt 27 de tari in UE)
+   let tari=tableData.map(elem=>elem.tara).slice(0,27);
+   //pentru fiecare tara creez randul din tabel
+   tari.forEach(element => {
       tr = document.createElement("tr");
       tdTara = document.createElement("td");
+      tdTara.innerHTML=element;
       tdSV = document.createElement("td");
       tdPIB = document.createElement("td");
       tdPop = document.createElement("td");
-      //cum verific daca exista deja tara?
-      tdTara.innerHTML = tableData[i].tara;
+      for(let index=0;index<tableData.length;index++){
+      if(tableData[index].tara===element)
+      {
+         tableData[index].indicator==="SV"? tdSV.innerHTML =(tableData[index].valoare!==null?tableData[index].valoare:"necunoscuta"):"";
+         tableData[index].indicator==="PIB"? tdPIB.innerHTML =tableData[index].valoare:"";
+         tableData[index].indicator==="POP"? tdPop.innerHTML =tableData[index].valoare:"";
+         
+         
+      }    
+      }  
       tr.appendChild(tdTara);
+      tr.appendChild(tdSV);
+      tr.appendChild(tdPIB);
+      tr.appendChild(tdPop);
+  
+      tBody.appendChild(tr);
+      table.appendChild(tBody);
+
+   });
+  // console.log(tari)
+  // for (i = 0; i < tableData.length; i++) {
+
+     // tr = document.createElement("tr");
+     // tdTara = document.createElement("td");
+     // tdSV = document.createElement("td");
+     // tdPIB = document.createElement("td");
+     // tdPop = document.createElement("td");
+      //cum verific daca exista deja tara?
+     // tdTara.innerHTML = tableData[i].tara;
+    //  tr.appendChild(tdTara);
       //cum pun indicatorul corespunzator tarii?
       //ceva nu e bineeeeeee !!!! 
-       console.log(tableData[i].indicator === "PIB");
-      tdSV.innerHTML = tableData[i].indicator === "SV" ? tableData[i].valoare : "";
-      tr.appendChild(tdSV);
-      tdPIB.innerHTML = tableData[i].indicator  === "PIB" ? tableData[i].valoare : "nothin";
-      tr.appendChild(tdPIB);
-      tdPop.innerHTML = tableData[i].indicator === "POP" ? tableData[i].valoare : "";
-      tr.appendChild(tdPop);
-      tBody.appendChild(tr);
+   //    console.log(tableData[i].indicator === "PIB");
+     // tdSV.innerHTML = tableData[i].indicator === "SV" ? tableData[i].valoare : "";
+    //  tr.appendChild(tdSV);
+      //tdPIB.innerHTML = tableData[i].indicator  === "PIB" ? tableData[i].valoare : "nothin";
+    //  tr.appendChild(tdPIB);
+     // tdPop.innerHTML = tableData[i].indicator === "POP" ? tableData[i].valoare : "";
+     // tr.appendChild(tdPop);
+     // tBody.appendChild(tr);
 
 
 
-   }
+   //}
 
-   table.appendChild(tBody);
+  //s table.appendChild(tBody);
    //adaugare tabel in pagina
    document.body.appendChild(table);
 }
@@ -93,8 +123,8 @@ async function main() {
    console.log(ani);
    let select = document.getElementById("ani");
    let selectTara = document.getElementById("selectTara");
-   select.add(new Option());
-   selectTara.add(new Option());
+   select.add(new Option("Selecteaza an"));
+   selectTara.add(new Option("Selecteaza tara"));
    //populare cu anii selectati din array
    for (let a in ani) {
       select.add(new Option(ani[a]));
@@ -102,13 +132,23 @@ async function main() {
    for (let t in tari) {
       selectTara.add(new Option(tari[t]));
    }
-   let anSelectat;
+   let btnDrawTable=document.getElementById("btnDeseneazaTabel");
+   let anSelectat=undefined;
    anSelectat = select.options[select.selectedIndex].value;
    select.addEventListener("change", function () {
       anSelectat = this.value;
-      drawTable(dataset, anSelectat);
+     
    })
-
+    btnDrawTable.addEventListener('click',function(){
+       console.log(anSelectat);
+       if(anSelectat!=="Selecteaza an"){
+         drawTable(dataset, anSelectat);
+       }
+       else{
+          alert("Selectati un an!");
+       }
+     
+    })
    //drawTable(dataset, anSelectat);
     let taraSelectata=selectTara.options[selectTara.selectedIndex];
     let indicatorSelectat;
