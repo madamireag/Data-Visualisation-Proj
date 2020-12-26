73d11db20b22e478d1cc66f23f0682a0305245f3
Mireag Madalina <mireagmada@gmail.com>
2020-12-26 19:00:39 +0200

    adaugare comentarii suplimentare in cod

--- 3_1082_Mireag_Madalina_Andreea.js
+++ 3_1082_Mireag_Madalina_Andreea.js
@@ -5,17 +5,20 @@ async function getDate() {
 }
 
 function drawTable(dataset, anSelectat) {
+   //daca tabelul exista deja il sterg si creez altul
    let existentTable = document.getElementById("dataTable");
    if (existentTable)
       document.body.removeChild(existentTable);
 
    let table = document.createElement("table");
-   table.setAttribute("id", "dataTable"); //ii dau id ca sa-l pot stiliza in css
+   table.setAttribute("id", "dataTable"); //ii dau id ca sa-l pot referi mai departe
    table.classList.add("table");
    //creare header tabel
    let tHead = document.createElement("thead");
+   //adaugare clasa de bootstrap
    tHead.classList.add("thead-dark");
    tHead.setAttribute("id", "tableHead");
+   //creez randul aferent headerului
    let rowHead = document.createElement("tr");
    let thTara = document.createElement("th");
    thTara.innerHTML = "Tara";
@@ -39,14 +42,13 @@ function drawTable(dataset, anSelectat) {
    let tdPIB;
    let tdPop;
    let tableData = [];
-   let SVvalues;
    let maxValue;
    let minValue;
    //filtrez datele si le iau doar pe cele care corespund anului selectat
    tableData = dataset.filter(e => e.an === anSelectat);
    //iau toate tarile intr-un vector(sunt 27 de tari in UE)
    let tari = tableData.map(elem => elem.tara).slice(0, 27);
-   //pentru fiecare tara creez randul din tabel
+   //pentru fiecare tara creez randul corespunzator din tabel
    tari.forEach(element => {
       tr = document.createElement("tr");
       tdTara = document.createElement("td");
@@ -60,6 +62,7 @@ function drawTable(dataset, anSelectat) {
                case "SV":
                   tdSV.innerHTML = (tableData[index].valoare !== null ? tableData[index].valoare : "-");
                   maxValue = Math.max(...tableData.filter(e => e.indicator === "SV").map(x => x.valoare));
+                  //colorare celule in functie de anumite intervale stabilite
                   minValue = Math.min(...tableData.filter(e => e.indicator === "SV" && e.valoare !== null).map(x => x.valoare));
                   if (tableData[index].valoare === maxValue) {
                      tdSV.style.backgroundColor = "green";
@@ -67,10 +70,10 @@ function drawTable(dataset, anSelectat) {
                   if (tableData[index].valoare === minValue) {
                      tdSV.style.backgroundColor = "red";
                   }
-                  if (tableData[index].valoare > minValue && tableData[index].valoare <= (minValue + maxValue) / 4) {
+                  if (tableData[index].valoare > minValue && tableData[index].valoare <= (minValue + maxValue) / 2) {
                      tdSV.style.backgroundColor = "#FC3";
                   }
-                  if (tableData[index].valoare > (minValue + maxValue) / 4 && tableData[index].valoare <= (minValue + maxValue) / 2) {
+                  if (tableData[index].valoare > (minValue + maxValue) / 4 && tableData[index].valoare <= (minValue + maxValue) / 2 && tableData[index].valoare !== minValue) {
                      tdSV.style.backgroundColor = "#FFA500";
                   }
                   if (tableData[index].valoare > (minValue + maxValue) / 2 && tableData[index].valoare < maxValue) {
@@ -81,6 +84,7 @@ function drawTable(dataset, anSelectat) {
                   tdPIB.innerHTML = (tableData[index].valoare !== null ? tableData[index].valoare : " - ");
                   maxValue = Math.max(...tableData.filter(e => e.indicator === "PIB").map(x => x.valoare));
                   minValue = Math.min(...tableData.filter(e => e.indicator === "PIB" && e.valoare !== null).map(x => x.valoare));
+                  //colorare celule in functie de anumite intervale stabilite
                   if (tableData[index].valoare === maxValue) {
                      tdPIB.style.backgroundColor = "green";
                   }
@@ -101,6 +105,7 @@ function drawTable(dataset, anSelectat) {
                   tdPop.innerHTML = (tableData[index].valoare !== null ? tableData[index].valoare : " - ");
                   maxValue = Math.max(...tableData.filter(e => e.indicator === "POP").map(x => x.valoare));
                   minValue = Math.min(...tableData.filter(e => e.indicator === "POP" && e.valoare !== null).map(x => x.valoare));
+                  //colorare celule in functie de anumite intervale stabilite
                   if (tableData[index].valoare === maxValue) {
                      tdPop.style.backgroundColor = "green";
                   }
@@ -108,13 +113,13 @@ function drawTable(dataset, anSelectat) {
                      tdPop.style.backgroundColor = "red";
                   }
                   if (tableData[index].valoare > minValue && tableData[index].valoare <= (minValue + maxValue) / 4) {
-                     tdPop.style.backgroundColor = "#FC3";
+                     tdPop.style.backgroundColor = "#FFA500"; //some kind of orange
                   }
                   if (tableData[index].valoare > (minValue + maxValue) / 4 && tableData[index].valoare <= (minValue + maxValue) / 2) {
-                     tdPop.style.backgroundColor = "#FFA500";
+                     tdPop.style.backgroundColor = "#FC3"; //galben-ish
                   }
                   if (tableData[index].valoare > (minValue + maxValue) / 2 && tableData[index].valoare < maxValue) {
-                     tdPop.style.backgroundColor = "#9C3";
+                     tdPop.style.backgroundColor = "#9C3"; //verde mai putin intens
                   }
 
             }
@@ -143,14 +148,38 @@ function drawChart(dataset, taraSelectata) {
    }
    //desenez un nou grafic 
    let barChart = new BarChart(chartDiv);
+   //filtrez datele pentru PIB -> nu iau in considerare valorile de null
    let date = dataset.filter(el => el.tara === taraSelectata && el.valoare != null).filter(el => el.indicator === "PIB");
    barChart.draw(date);
 }
 //TO DO
 function drawBubbleChart(dataset, anSelectat) {
    let canvas = document.getElementById("canvas_chart");
+   canvas.style.display = "inline-block";
    let context = canvas.getContext("2d");
+
+
+   context.fillStyle = "whitesmoke";
    context.fillRect(0, 0, canvas.width, canvas.height);
+   canvas.style.border = "1px solid black";
+   context.fillStyle = "#696969";
+   let SVdata = dataset.filter(e => e.an === anSelectat).filter(e => e.indicator === "SV");
+   let tari = SVdata.map(elem => elem.tara).slice(0, 27);
+   tari.forEach(element => {
+      for (let index = 0; index < SVdata.length; index++) {
+         if (tableData[index].tara === element) {
+
+            const bubbleY = SVdata[index].valoare * 0.9;
+            context.beginPath();
+
+            context.endPath();
+         }
+      }
+   })
+
+
+
+
 }
 
 
@@ -175,7 +204,7 @@ async function main() {
    let selectTara = document.getElementById("selectTara");
    select.add(new Option("Selecteaza an"));
    selectTara.add(new Option("Selecteaza tara"));
-   //populare cu anii/tarile din array
+   //populare select cu anii/tarile din array-uri
    for (let a in ani) {
       select.add(new Option(ani[a]));
    }
@@ -189,20 +218,20 @@ async function main() {
    let anSelectat = undefined;
    let taraSelectata = undefined;
 
-   //abonare la evenimentul de change=>cand user-ul alege un an apar butoanele cu optiunile corespunzatoare si se salveaza anul 
+   //abonare la evenimentul de change => cand utilizatorul alege un an apar butoanele cu optiunile corespunzatoare si se salveaza anul 
    select.addEventListener("change", function () {
       anSelectat = this.value;
       btnDrawTable.style.display = "inline-block";
       btnDrawBublechart.style.display = "inline-block";
 
    });
-   //abonare la evenimentul de change=>cand user-ul alege o tara apare butonul cu optiunea corespunzatoare si se salveaza tara 
+   //abonare la evenimentul de change => cand utilizatorul alege o tara apare butonul cu optiunea corespunzatoare si se salveaza tara 
    selectTara.addEventListener("change", function () {
       taraSelectata = this.value;
       btnBarchart.style.display = "inline-block";
 
    })
-   //tratare situatie in care user-ul apasa pe un buton fara un an selectat
+   //tratare eveniment de click pe buton si situatie in care user-ul apasa pe un buton fara un an selectat
    btnDrawTable.addEventListener('click', function () {
       if (anSelectat !== "Selecteaza an") {
          drawTable(dataset, anSelectat);
@@ -212,7 +241,7 @@ async function main() {
       }
 
    })
-   //tratare situatie in care user-ul apasa pe buton fara o tara selectata
+   //tratare eveniment de click pe buton si situatie in care user-ul apasa pe buton fara o tara selectata
    btnBarchart.addEventListener("click", function () {
       if (taraSelectata !== "Selecteaza tara") {
          drawChart(dataset, taraSelectata);
@@ -228,6 +257,7 @@ async function main() {
 
 }
 
+
 class BarChart {
    constructor(element) {
       this.element = element;
@@ -242,17 +272,22 @@ class BarChart {
       this.drawBackground();
       this.drawBars(data);
    }
+
    createSVG() {
+      //creare element svg pe baza namespace-ului
       this.svg = document.createElementNS(this.svgns, "svg");
+      //adaugare border pentru element
       this.svg.style.borderColor = "black";
       this.svg.style.borderWidth = "1px";
       this.svg.style.borderStyle = "solid";
       this.svg.setAttribute("width", this.width);
       this.svg.setAttribute("height", this.height);
+      //setare id pentru svg pentru a fi referit ulterior
       this.svg.setAttribute("id", "svgChart");
       this.element.appendChild(this.svg);
    }
    drawBackground() {
+      //desenare dreptunghi pe toata suprafata elementului
       const rect = document.createElementNS(this.svgns, "rect");
       rect.setAttribute("x", 0);
       rect.setAttribute("y", 0);
@@ -264,16 +299,20 @@ class BarChart {
    }
    drawTitle(tara, maxBarHeight) {
       const titlu = document.createElementNS(this.svgns, "text");
+      //setare continut pentru titlu (text)
       titlu.appendChild(document.createTextNode("Evolutie PIB in " + tara));
+      //stilizare font si culoare pentru titlu
       titlu.setAttribute("font-style", "italic");
       titlu.style.fill = "#696969";
       titlu.setAttribute("font-family", "sans-serif");
+      //setare coordonate pentru titlu (in centru pe orizontala si pe verticala calculat in functie de inaltimea maxima a barelor)
       titlu.setAttribute("x", this.element.clientWidth / 2);
       titlu.setAttribute("y", this.element.clientHeight - maxBarHeight - 30);
 
       this.svg.appendChild(titlu);
    }
    drawBars() {
+      //latimea unei bare
       const barWidth = this.width / this.data.length;
 
       const maxValue = Math.max(...this.data.map(x => x.valoare));
@@ -284,7 +323,7 @@ class BarChart {
          const value = this.data[i].valoare;
          var tara = this.data[i].tara;
          const barHeight = value * f * 0.9;
-         console.log(barHeight);
+         //coordonatele x si y pentru bare
          const barX = i * barWidth;
          const barY = this.height - barHeight;
 
@@ -294,12 +333,13 @@ class BarChart {
          bar.setAttribute("y", barY - 20);
          bar.setAttribute("width", barWidth / 2);
          bar.setAttribute("height", barHeight);
+         //colorez bara corespunzatoare valorii maxime diferit
          if (value === maxValue) {
             bar.style.fill = "#724166";
          } else {
             bar.style.fill = "tomato";
          }
-         //salvez barHeight pentru elementul maxim (var -> nu vreau block-scope in acest caz)
+         //salvez barHeight pentru elementul cu valoare maxima (var -> nu vreau block-scope in acest caz)
          var maxBarHeight;
          if (value == maxValue) {
             console.log("hello")
@@ -307,9 +347,8 @@ class BarChart {
          }
 
          bar.style.stroke = "black";
-         let strWidth = "stroke-width";
          bar.style["stroke-width"] = "1px";
-         //abonare la evenimentul de mouseover si afisare tooltip
+         //abonare la evenimentul de mouseover si afisare tooltip (browserele afiseaza elementul de tip title ca si un tooltip)
          bar.addEventListener('mouseover', (ev) => {
             let title = document.createElementNS(this.svgns, "title");
             title.textContent = `PIB: ${value} Anul:${label}`;
